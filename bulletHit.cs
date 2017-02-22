4017a0ceb21fb251ac6b4d50494ef7205b38e42c
Lasley Quinn <3933904277@r9tigermail.org>
2017-02-22 14:55:35 -0600

    Added Slow Enemy Script and Other Updates

--- bulletHit.cs
+++ bulletHit.cs
@@ -5,6 +5,13 @@ public class bulletHit : MonoBehaviour {
 
     playerScript pS;
 
+    slowScript sS;
+
+    public const int NORMAL_SCORE = 10;
+    public const int FINAL_SCORE = 25;
+    public const int SLOW_SCORE = 15;
+    public const int SLOW_HP = 3;
+
     // Use this for initialization
     void Start () {
         GameObject player = GameObject.Find("Player");
@@ -21,13 +28,23 @@ public class bulletHit : MonoBehaviour {
         if (coll.gameObject.tag == "Enemy")
         {
             Destroy(coll.gameObject);
-            pS.Score += 10;
+            pS.Score += NORMAL_SCORE;
         }
         if (coll.gameObject.tag == "Final Enemy")
         {
             Destroy(coll.gameObject);
-            pS.Score += 25;
+            pS.Score += FINAL_SCORE;
             Application.LoadLevel(Application.loadedLevel + 1);
+        }
+        if (coll.gameObject.tag == "Slow Enemy")
+        {
+            Debug.Log(coll.gameObject.GetComponent<slowScript>().hp);
+
+            if (coll.gameObject.GetComponent<slowScript>().hp == 0)
+            {
+                Destroy(coll.gameObject);
+                pS.Score += SLOW_SCORE;
+            }
         }
     }
 }
---
+++ slowScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slowScript : MonoBehaviour {
+
+    public int hp = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    int getHp()
+    {
+        return hp;
+    }
+
+    void setHp(int hp)
+    {
+        this.hp = hp;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Bullet")
+        {
+            hp--;
+            Destroy(coll.gameObject);
+        }
+    }
+}
